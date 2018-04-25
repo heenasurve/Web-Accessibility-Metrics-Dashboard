874f563b90b53282ec7dea1119f762cd19db159c
KhalidSAlmalki <vb.95@hotmail.com>
2018-04-25 14:09:24 -0400

    delete unwanted files and add the finall result of 300 websites

--- AccessibilityMetrics/getAccessibilityIssues.js
+++ AccessibilityMetrics/getAccessibilityIssues.js
@@ -8,70 +8,13 @@ const opts = { chromeFlags: ['--show-paint-rects'] };
 
 const fs = require('fs');
 
-let common_mistakes = require("./accessibility_common_issues.json");
 
-
-//ranking
-// "http://www.rit.edu/",
-//     "https://www.princeton.edu",
-//     "https://www.harvard.edu",
-//     "https://www.uchicago.edu",
-//     "https://www.columbia.edu",
-//     "https://ucsd.edu",
-//     "https://www1.lehigh.edu",
-//     "http://www.psu.edu",
-//     "https://www.fordham.edu",
-//     "https://www.umass.edu",
-//     "https://www.iastate.edu",
-//     "https://www.catholic.edu/index.html",
-//     "https://www.colostate.edu",
-//     "https://www2.gmu.edu",
-//     "https://www.k-state.edu",
-//     "https://www.wright.edu",
-//     "https://www.umkc.edu",
-//     "http://wmich.edu",
-//     "http://www.rit.edu/",
-//     "https://www.umich.edu/",
-//     "https://www.usc.edu/",
-//     "http://www.northeastern.edu/",
-//     "https://www.dean.edu/"
-
-
-//Software Engineering Research University
-// "http://www.se.rit.edu/",
-//     "https://msse.sjsu.edu/",
-//     "https://csc.calpoly.edu/",
-//     "https://www.stevens.edu/school-systems-enterprises/masters-degree-programs/software-engineering",
-//     "http://www.byui.edu/computer-science-electrical-engineering/degrees-and-majors/b-s-in-software-engineering"
-
-//International
-
-// "http://www.rit.edu/",
-//     "https://www.cam.ac.uk/",
-//         "https://www.utoronto.ca/",
-//             "https://www.ethz.ch/en.html",
-//                 "http://www.iitd.ac.in/",
-//                     "http://www.tsinghua.edu.cn/publish/newthuen/"
-
-//Within the same university
-
-// "http://www.se.rit.edu/",
-//     "http://cis.rit.edu/",
-// "http://cias.rit.edu/",
-//             "http://nsa.rit.edu/",
-//                 "https://www.rit.edu/science/"
 
 let university_websites= [
-
-    "http://www.se.rit.edu/",
-    "http://cis.rit.edu/",
-    "http://cias.rit.edu/",
-    "http://nsa.rit.edu/",
-    "https://www.rit.edu/science/"
+    "http://www.tnstate.edu","http://www.tntech.edu","http://www.tamuc.edu","http://www.tamucc.edu","http://www.tamuk.edu","http://tsu.edu","http://txstate.edu","https://twu.edu","https://www.trevecca.edu","https://www.tiu.edu","http://www.uakron.edu","http://ualr.edu","https://louisiana.edu","http://www.ulm.edu","https://www.umes.edu/home/","http://www.memphis.edu","https://www.umsl.edu","https://www.unomaha.edu","https://www.unlv.edu","http://uno.edu","https://www.uncg.edu","http://unco.edu","http://unt.edu","http://southalabama.edu","http://usm.edu","https://www.uta.edu/uta/","https://www.utep.edu","http://www.utrgv.edu/en-us/","https://www.utsa.edu","http://ucumberlands.edu","http://utoledo.edu","http://uwf.edu","http://westga.edu","http://uwm.edu","http://valdosta.edu","http://wichita.edu","http://wright.edu"
 
    ];
 
-let audits = common_mistakes.common_issues;
 
 function launchChromeAndRunLighthouse(url, opts, config){
     return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
@@ -86,49 +29,67 @@ function launchChromeAndRunLighthouse(url, opts, config){
 let allData = []
 
 function analyzeWebsite(index) {
-    if (index < university_websites.length){
-        let website = university_websites[index];
+    try {
 
-        launchChromeAndRunLighthouse(website,opts).then(results => {
+        if (index < university_websites.length){
+            let website = university_websites[index];
 
-
-            let  reportCategories =  results["reportCategories"];
-
-
-            reportCategories.forEach(function (val) {
-
-                if (val["name"] == "Accessibility"){
-
-                    let accessibility_audits = val["audits"];
-
-                     let neededData = JSON.stringify({websiteName:website,audits:accessibility_audits});
-
-                    allData.push(neededData);
-
-                    analyzeWebsite(index = index+1);
+            launchChromeAndRunLighthouse(website,opts).then(results => {
 
 
-                }
+                let  reportCategories =  results["reportCategories"];
+
+
+                reportCategories.forEach(function (val) {
+
+                    if (val["name"] == "Accessibility"){
+
+                        let accessibility_audits = val["audits"];
+
+                        let neededData = JSON.stringify({websiteName:website,audits:accessibility_audits});
+
+                        allData.push(neededData);
+
+                        writeResult(allData,website)
+
+                        analyzeWebsite(index = index+1);
+
+
+                    }
+
+                });
+
 
             });
 
+        }else {
 
-        });
+         //   writeResult(allData)
+        }
 
-     }else {
-        fs.writeFile('Withinthesameuniversity.json', allData, function (err){
-            if (err){
-                return console.log("error happened ");
-            }
+    }catch (ex)  {
+        console.log("ex ex",ex);
 
-            return console.log("All data Saved");
+        writeResult(allData)
+        analyzeWebsite(index = index+1);
 
-        });
     }
 
 
+
 }
-// analyzeWebsite(0)
+function writeResult(data ,websiteName) {
+
+    fs.writeFile('Rankings3.json', data, function (err){
+        if (err){
+            return console.log("error happened ");
+        }
+
+        return console.log(websiteName+" was Saved");
+
+    });
+}
+ //analyzeWebsite(0)
 
 function readParseFile(fileName) {
 
@@ -144,12 +105,19 @@ function readParseFile(fileName) {
                         let websiteName = val["websiteName"]
                         let audits = val["audits"];
 
+
+                         var temp = ""+websiteName+",";
+
                         audits.forEach(function (value) {
 
 
-                            console.log(websiteName+" , "+value.id+","+value.score+","+ "'"+value.result.helpText+"'")
+                            temp = temp+value.score
+                            temp = temp+","
 
                         })
+                       temp = temp.substring(0, temp.length - 1);
+
+                        console.log(temp)
 
 
                     });
@@ -157,40 +125,5 @@ function readParseFile(fileName) {
             });
 
 }
-//readParseFile("Ranking");
-//readParseFile("SoftwareEngineeringResearchUniversity");
-// readParseFile("International");
-//readParseFile("Withinthesameuniversity");
+readParseFile("Rankings");
 
-
-
-// university_websites.forEach(function(website){
-//
-//       launchChromeAndRunLighthouse(website, opts).then(results => {
-//         let response = JSON.stringify(results);
-//         fs.writeFile('response.json', response, function (err){
-//             if (err)
-//                 return console.log(err);
-//                  console.log(website);
-//
-//             // fs.readFile('./response.json', 'utf8', function (err,data) {
-//             //     if (err) {
-//             //         return console.log(err);
-//             //     }
-//             //     let JSON_data = JSON.parse(data);
-//             //     let categories = JSON_data["reportCategories"];
-//             //     let category_info = "";
-//             //     categories.forEach(function(category){
-//             //        console.log(category.name + "-" + category.score);
-//             //     });
-//             //     let accessibility_audits = categories[2].audits;
-//             //     audits.forEach(function(audit){
-//             //
-//             //         fs.writeFileSync('student-2.json', response);
-//             //         console.log(accessibility_audits[Object.keys(audit)[0]].id +"->" + accessibility_audits[Object.keys(audit)[0]].score);
-//             //     });
-//             //
-//             // });
-//         });
-//     });
-// });
